bb765dfb8d2722e7c825948386763a1f548dca4b
Nitish Kumar Gupta <146057656+nitish-kumar-gupta@users.noreply.github.com>
2026-09-18 13:43:14 +0530

    Add files via upload

---
+++ StreamlitApp.py
@@ -0,0 +1,93 @@
+import os
+import json
+import traceback
+import pandas as pd
+from dotenv import load_dotenv
+from mcqgenerator.utils import read_file,get_table_data
+import streamlit as st
+from langchain.callbacks import get_openai_callback
+from mcqgenerator.mcqgenerator import generate_evaluate_chain
+from mcqgenerator.logger import logging
+
+#loading json file
+
+with open('Response.json', 'r') as file:
+    RESPONSE_JSON = json.load(file)
+
+#creating a title for the app
+st.write("<h1 style='text-align: center;'>🤖 MCQs Creator Application With LangChain And OpenAI 🤖</h1>",unsafe_allow_html=True)
+st.caption("Note: This app act as an expert MCQs maker, it is assumed that you provide the appropriate subject related to data to generate the quiz for students.")
+
+#Create a form using st.form
+with st.form("user_inputs"):
+    #File Upload
+    uploaded_file=st.file_uploader("Uplaod a PDF or TXT file")
+
+    #Input Fields
+    mcq_count=st.number_input("Number of MCQs", min_value=3, max_value=50)
+
+    #Subject
+    subject=st.text_input("Insert Subject",max_chars=20)
+
+    # Quiz Tone
+    tone=st.text_input("Complexity Level Of Questions", max_chars=20, placeholder="Simple")
+
+    #Add Button
+    button=st.form_submit_button("Create MCQs")
+
+    # Check if the button is clicked and all fields have input
+
+    if button and uploaded_file is not None and mcq_count and subject and tone:
+        with st.spinner("loading..."):
+            try:
+                text=read_file(uploaded_file)
+                #Count tokens and the cost of API call
+                with get_openai_callback() as cb:
+                    response=generate_evaluate_chain(
+                        {
+                        "text": text,
+                        "number": mcq_count,
+                        "subject":subject,
+                        "tone": tone,
+                        "response_json": json.dumps(RESPONSE_JSON)
+                            }
+                    )
+                #st.write(response)
+
+            except Exception as e:
+                traceback.print_exception(type(e), e, e.__traceback__)
+                st.error("Error")
+
+            else:
+                print(f"Total Tokens:{cb.total_tokens}")
+                print(f"Prompt Tokens:{cb.prompt_tokens}")
+                print(f"Completion Tokens:{cb.completion_tokens}")
+                print(f"Total Cost:{cb.total_cost}")
+                if isinstance(response, dict):
+                    #Extract the quiz data from the response
+                    quiz=response.get("quiz", None)
+                    if quiz is not None:
+                        table_data=get_table_data(quiz)
+                        if table_data is not None:
+                            df=pd.DataFrame(table_data)
+                            df.index=df.index+1
+                            st.table(df)
+
+                            
+
+                            #Display the review in a text box as well
+                            st.text_area(label="Review", value=response["review"])
+                        else:
+                            st.error("Error in the table data")
+
+                else:
+                    st.write(response)
+
+if 'df' in locals():
+    download_button = st.download_button(
+        label="Download MCQ Data",
+        data=df.to_csv().encode('utf-8'),
+        file_name=f"{subject}_MCQ_Data.csv",
+        key='download_button'
+    )
+
